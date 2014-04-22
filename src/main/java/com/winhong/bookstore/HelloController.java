7425859c25a779844a5cc94a17163b7f5a2a0522
XiongNeng <yidao620@gmail.com>
2014-04-22 18:05:56 +0800

    2222

--- src/main/java/com/winhong/bookstore/HelloController.java
+++ src/main/java/com/winhong/bookstore/HelloController.java
@@ -8,9 +8,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HelloController {
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET, value = "hello")
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!!!哈哈哈");
 		return "hello";
 	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "sitemesh")
+	public String sitemesh(ModelMap model) {
+		model.addAttribute("message", "Hello world!!!哈哈哈");
+		return "sitemesh/menu";
+	}
+
 }
